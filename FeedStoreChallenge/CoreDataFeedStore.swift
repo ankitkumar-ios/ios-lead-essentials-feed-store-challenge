8aa66778b346b0164ac0b023f66eb10dde699f69
Ankit Kumar <ankit.kumar@truminds.com>
2021-07-30 08:41:50 +0530

    Delivers found values on non empty cache

--- FeedStoreChallenge/CoreDataFeedStore.swift
+++ FeedStoreChallenge/CoreDataFeedStore.swift
@@ -29,14 +29,67 @@ public final class CoreDataFeedStore: FeedStore {
 	}
 
 	public func retrieve(completion: @escaping RetrievalCompletion) {
-		completion(.empty)
+		let context = self.context
+		context.perform {
+			do {
+				let request = NSFetchRequest<ManagedCache>(entityName: ManagedCache.entity().name!)
+				request.returnsObjectsAsFaults = false
+				let result = try context.fetch(request).first
+				guard let eResult = result else {
+					completion(.empty)
+					return
+				}
+				let feed = eResult.feed.compactMap { ($0 as? ManagedFeedImage)?.local }
+				completion(.found(feed: feed, timestamp: eResult.timestamp))
+			} catch {
+				completion(.failure(error))
+			}
+		}
 	}
 
 	public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-		fatalError("Must be implemented")
+		let context = self.context
+		context.perform {
+			let managedCache = ManagedCache(context: context)
+
+			let orderedSet = NSOrderedSet(array: feed.map { item in
+				let managed = ManagedFeedImage(context: context)
+				managed.id = item.id
+				managed.feed_description = item.description
+				managed.location = item.location
+				managed.url = item.url
+
+				return managed
+			})
+
+			managedCache.timestamp = timestamp
+			managedCache.feed = orderedSet
+
+			context.insert(managedCache)
+			completion(nil)
+		}
 	}
 
 	public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
 		fatalError("Must be implemented")
+	}
+}
+
+@objc(ManagedCache)
+private class ManagedCache: NSManagedObject {
+	@NSManaged internal var timestamp: Date
+	@NSManaged internal var feed: NSOrderedSet
+}
+
+@objc(ManagedFeedImage)
+private class ManagedFeedImage: NSManagedObject {
+	@NSManaged internal var id: UUID
+	@NSManaged internal var feed_description: String?
+	@NSManaged internal var location: String?
+	@NSManaged internal var url: URL
+	@NSManaged internal var cache: ManagedCache
+
+	var local: LocalFeedImage {
+		return LocalFeedImage(id: id, description: feed_description, location: location, url: url)
 	}
 }
