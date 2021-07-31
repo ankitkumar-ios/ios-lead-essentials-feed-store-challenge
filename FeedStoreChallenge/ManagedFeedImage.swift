c117cacf993fc27f7059d6de4cd083a85a8ab3a2
Ankit Kumar <ankit.kumar@truminds.com>
2021-07-31 07:39:26 +0530

    Removed internal access level, as it's also the default

--- FeedStoreChallenge/ManagedFeedImage.swift
+++ FeedStoreChallenge/ManagedFeedImage.swift
@@ -9,18 +9,18 @@
 import CoreData
 
 @objc(ManagedFeedImage)
-internal class ManagedFeedImage: NSManagedObject {
-	@NSManaged internal var id: UUID
-	@NSManaged internal var feed_description: String?
-	@NSManaged internal var location: String?
-	@NSManaged internal var url: URL
-	@NSManaged internal var cache: ManagedCache
+class ManagedFeedImage: NSManagedObject {
+	@NSManaged var id: UUID
+	@NSManaged var feed_description: String?
+	@NSManaged var location: String?
+	@NSManaged var url: URL
+	@NSManaged var cache: ManagedCache
 
 	var local: LocalFeedImage {
 		return LocalFeedImage(id: id, description: feed_description, location: location, url: url)
 	}
 
-	internal static func image(from localFeed: [LocalFeedImage], in context: NSManagedObjectContext) -> NSOrderedSet {
+	static func image(from localFeed: [LocalFeedImage], in context: NSManagedObjectContext) -> NSOrderedSet {
 		return NSOrderedSet(array: localFeed.map { item in
 			let managed = ManagedFeedImage(context: context)
 			managed.id = item.id
